e13b4868d9daaf9c8d41aa9da260ecc01674c192
deepika731 <33340325+deepika731@users.noreply.github.com>
2019-11-21 10:16:46 +0530

    Add files via upload

---
+++ temp_app.py
@@ -0,0 +1,211 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+tf.compat.v1.enable_eager_execution()
+from flask import Flask, render_template
+import os
+from flask import *
+import IPython.display as display
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12,12)
+mpl.rcParams['axes.grid'] = False
+import numpy as np
+import PIL.Image
+import time
+import functools
+import itertools
+import keras
+from vgg19 import VGG19
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+os.environ['TF_KERAS'] = '1'
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+  new_shape = tf.cast(shape * scale, tf.int32)
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+def imshow(image, title=None):
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
+
+  plt.imshow(image)
+  if title:
+    plt.title(title)
+
+
+def vgg_layers(layer_names):
+  """ Creates a vgg model that returns a list of intermediate output values."""
+  # Load our model. Load pretrained VGG, trained on imagenet data
+  vgg = VGG19(include_top=False, weights='imagenet')
+  vgg.trainable = False
+  
+  outputs = [vgg.get_layer(name).output for name in layer_names]
+
+  model = keras.Model([vgg.input], outputs)
+  return model
+
+
+def gram_matrix(input_tensor):
+  result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+  input_shape = tf.shape(input_tensor)
+  num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+  return result/(num_locations)
+
+class StyleContentModel(tf.keras.models.Model):
+  def __init__(self, style_layers, content_layers):
+    super(StyleContentModel, self).__init__()
+    self.vgg =  vgg_layers(style_layers + content_layers)
+    self.style_layers = style_layers
+    self.content_layers = content_layers
+    self.num_style_layers = len(style_layers)
+    self.vgg.trainable = False
+  def call(self, inputs):
+    "Expects float input in [0,1]"
+    inputs = inputs*255.0
+    preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+    outputs = self.vgg(preprocessed_input)
+    style_outputs, content_outputs = (outputs[:self.num_style_layers], 
+                                      outputs[self.num_style_layers:])
+
+    style_outputs = [gram_matrix(style_output)
+                     for style_output in style_outputs]
+
+    content_dict = {content_name:value 
+                    for content_name, value 
+                    in zip(self.content_layers, content_outputs)}
+
+    style_dict = {style_name:value
+                  for style_name, value
+                  in zip(self.style_layers, style_outputs)}
+    
+    return {'content':content_dict, 'style':style_dict}
+
+def clip_0_1(image):
+  return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+
+
+
+PEOPLE_FOLDER = os.path.join('static', 'uploads')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
+
+@app.route('/')
+def index():
+	return render_template("frontend.html")
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f1 = request.files['image1']  
+        f1.save(os.path.join(app.config['UPLOAD_FOLDER'], f1.filename))
+        f2 = request.files['image2']
+        f2.save(os.path.join(app.config['UPLOAD_FOLDER'], f2.filename))
+        style_path = f2.filename
+        content_path = f1.filename
+        content_image = load_img(content_path)
+        style_image = load_img(style_path)
+        x = tf.keras.applications.vgg19.preprocess_input(content_image*255)
+        x = tf.image.resize(x, (224, 224))
+        vgg = VGG19(include_top=True, weights='imagenet')
+        prediction_probabilities = vgg(x)
+        #prediction_probabilities.shape
+        predicted_top_5 = tf.keras.applications.vgg19.decode_predictions(prediction_probabilities.numpy())[0]
+        [(class_name, prob) for (number, class_name, prob) in predicted_top_5]
+        print(predicted_top_5)
+        vgg = VGG19(include_top=False, weights='imagenet')
+        print()
+        for layer in vgg.layers:
+        	print(layer.name)
+        # Content layer where will pull our feature maps
+        content_layers = ['block5_conv2']
+        # Style layer of interes
+        style_layers = ['block1_conv1',
+		        'block2_conv1',
+		        'block3_conv1', 
+		        'block4_conv1', 
+		        'block5_conv1']
+        num_content_layers = len(content_layers)
+        num_style_layers = len(style_layers)
+        style_extractor = vgg_layers(style_layers)
+        style_outputs = style_extractor(style_image*255)
+        #Look at the statistics of each layer's output
+        for name, output in zip(style_layers, style_outputs):
+        	print(name)
+        	print("  shape: ", output.numpy().shape)
+        	print("  min: ", output.numpy().min())
+        	print("  max: ", output.numpy().max())
+        	print("  mean: ", output.numpy().mean())
+        	print()
+        extractor = StyleContentModel(style_layers, content_layers)
+        results = extractor(tf.constant(content_image))
+        style_results = results['style']
+        print('Styles:')
+        for name, output in sorted(results['style'].items()):
+        	print("  ", name)
+        	print("    shape: ", output.numpy().shape)
+        	print("    min: ", output.numpy().min())
+        	print("    max: ", output.numpy().max())
+        	print("    mean: ", output.numpy().mean())
+        print("Contents:")
+        for name, output in sorted(results['content'].items()):
+        	print("  ", name)
+        	print("    shape: ", output.numpy().shape)
+        	print("    min: ", output.numpy().min())
+        	print("    max: ", output.numpy().max())
+        	print("    mean: ", output.numpy().mean())
+
+        style_targets = extractor(style_image)['style']
+        content_targets = extractor(content_image)['content']
+        image = tf.Variable(content_image)
+        style_weight=1e-2
+        content_weight=1e4
+        opt = tf.keras.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+        def style_content_loss(outputs):
+        	style_outputs = outputs['style']
+        	content_outputs = outputs['content']
+        	style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) 
+                           for name in style_outputs.keys()])
+        	style_loss *= style_weight / num_style_layers
+
+        	content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                             for name in content_outputs.keys()])
+        	content_loss *= content_weight / num_content_layers
+        	loss = style_loss + content_loss
+        	return loss
+        @tf.function()
+        def train_step(image):
+        	with tf.GradientTape() as tape:
+        		outputs = extractor(image)
+        		loss = style_content_loss(outputs)
+        	grad = tape.gradient(loss, image)
+        	opt.apply_gradients([(grad, image)])
+        	image.assign(clip_0_1(image))
+        for i in range(100):
+        	train_step(image)
+        img = tensor_to_image(image)
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'], 'output.jpg'))
+        full_filename = {}
+        full_filename[0] = os.path.join(app.config['UPLOAD_FOLDER'], f1.filename)
+        full_filename[1] = os.path.join(app.config['UPLOAD_FOLDER'], f2.filename)
+        full_filename[2] = os.path.join(app.config['UPLOAD_FOLDER'], 'output.jpg')
+        return render_template("output.html", user_image = full_filename)
+if __name__ == '__main__':
+   app.run(debug = True)
